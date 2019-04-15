ad68b18ae0777662eb783a45d0233cd4d5479199
tonycatgogogo <gonghaitao1@gmail.com>
2019-04-15 14:45:25 +0800

    2019.4.15提交by hito

--- src/components/Goods/GoodsList.vue
+++ src/components/Goods/GoodsList.vue
@@ -1,6 +1,101 @@
 <template>
-    <div>
-        <h1>商品页</h1>
+    <div class="good-list">
+        <div class="good-item">
+            <img
+                src="https://img11.360buyimg.com/n1/s450x450_jfs/t1/5479/8/3546/70556/5b997c0cE40f64978/6540f1c2a6090257.jpg"
+                alt="">
+            <h1 class="title">小米（Mi）小米Note 16G双网通版</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">¥899</span>
+                    <span class="old">¥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="good-item">
+            <img
+                src="https://img14.360buyimg.com/n1/s450x450_jfs/t1/33346/26/1398/278371/5ca57160Ea53e0265/38d2d8705211b798.jpg"
+                alt="">
+            <h1 class="title">尼康(Nikon)D3300套机（18-55mm f/3.5-5.6G VRII）（黑色）</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">¥899</span>
+                    <span class="old">¥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="good-item">
+            <img
+                src="https://img14.360buyimg.com/n1/s450x450_jfs/t1/33346/26/1398/278371/5ca57160Ea53e0265/38d2d8705211b798.jpg"
+                alt="">
+            <h1 class="title">小米（Mi）小米Note 16G双网通版</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">¥899</span>
+                    <span class="old">¥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="good-item">
+            <img
+                src="https://img14.360buyimg.com/n1/s450x450_jfs/t1/33346/26/1398/278371/5ca57160Ea53e0265/38d2d8705211b798.jpg"
+                alt="">
+            <h1 class="title">小米（Mi）小米Note 16G双网通版</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">¥899</span>
+                    <span class="old">¥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="good-item">
+            <img
+                src="https://img14.360buyimg.com/n1/s450x450_jfs/t1/33346/26/1398/278371/5ca57160Ea53e0265/38d2d8705211b798.jpg"
+                alt="">
+            <h1 class="title">小米（Mi）小米Note 16G双网通版</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">¥899</span>
+                    <span class="old">¥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
+        <div class="good-item">
+            <img
+                src="https://img14.360buyimg.com/n1/s450x450_jfs/t1/33346/26/1398/278371/5ca57160Ea53e0265/38d2d8705211b798.jpg"
+                alt="">
+            <h1 class="title">小米（Mi）小米Note 16G双网通版</h1>
+            <div class="info">
+                <p class="price">
+                    <span class="now">¥899</span>
+                    <span class="old">¥999</span>
+                </p>
+                <p class="sell">
+                    <span>热卖中</span>
+                    <span>剩60件</span>
+                </p>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,5 +104,52 @@
 </script>
 
 <style lang="scss" scoped>
+    .good-list {
+        display: flex;
+        flex-wrap: wrap;
+        padding: 7px;
+        justify-content: space-between;
 
+        .good-item {
+            width: 49%;
+            border: 1px solid #ccc;
+            box-shadow: 0 0 8px #999;
+            margin: 4px 0;
+            padding: 2px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            min-height: 264px;
+            img {
+                width: 100%;
+            }
+            .title {
+                font-size: 14px;
+            }
+            .info {
+                background-color: rgba(255, 255, 0, 0.3);
+                p {
+                    margin: 0;
+                    padding: 5px;
+                }
+                .price {
+                    .now {
+                        color: red;
+                        font-weight: 700;
+                        font-size: 16px;
+                    }
+                    .old {
+                        text-decoration: line-through;
+                        font-size: 12px;
+                        margin-left: 10px;
+                    }
+                }
+                .sell {
+                    display: flex;
+                    justify-content: space-between;
+                    font-size: 13px;
+                }
+            }
+        }
+    }
 </style>
--- src/components/photos/PhotoList.vue
+++ src/components/photos/PhotoList.vue
@@ -36,7 +36,7 @@
                 <img v-lazy="item.img_url">
                 <div class="info">
                     <h1 class="info-title">{{ item.title }}</h1>
-                    <div class="info-body">{{ item.zhaiyao }}</div>
+                    <div class="info-body">{{ item.describe }}</div>
                 </div>
             </router-link>
         </ul>
@@ -53,37 +53,55 @@
           id: 50,
           title: '有气质且带可爱的美女组图',
           img_url: 'http://img4.imgtn.bdimg.com/it/u=2153937626,1074119156&fm=26&gp=0.jpg',
-          zhaiyao: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
+          describe: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
         }, {
           id: 51,
           title: '有气质且带可爱的美女组图',
           img_url: 'http://img0.imgtn.bdimg.com/it/u=1393771583,1541386921&fm=26&gp=0.jpg',
-          zhaiyao: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
+          describe: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
         }, {
           id: 53,
           title: '有气质且带可爱的美女组图',
           img_url: 'http://img1.imgtn.bdimg.com/it/u=282564059,849247250&fm=26&gp=0.jpg',
-          zhaiyao: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
+          describe: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
         }, {
           id: 54,
           title: '有气质且带可爱的美女组图',
           img_url: 'http://img2.imgtn.bdimg.com/it/u=2309772032,1565890452&fm=200&gp=0.jpg',
-          zhaiyao: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
+          describe: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
         }, {
           id: 55,
           title: '有气质且带可爱的美女组图',
           img_url: 'http://img4.imgtn.bdimg.com/it/u=2291382945,2489252111&fm=200&gp=0.jpg',
-          zhaiyao: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
+          describe: '有气质且带可爱的美女组图有气质且带可爱的美女组图'
         }]
       }
     },
     created() {
-
+      this.getAllCategory();
+      this.getPhotoListByCateId(0)
     },
     mounted() {
       mui(".mui-scroll-wrapper").scroll({
         deceleration: 0.0005
       });
+    },
+    methods: {
+      getAllCategory() {
+        this.$http.get("api/getimgcategory").then(result => {
+          if (result.body.status === 0) {
+            result.body.message.unshift({title: '全部', id: 0})
+            this.cates = result.body.message
+          }
+        })
+      },
+      getPhotoListByCateId(cateId) {
+        this.$http.get('api/getimages/' + cateId).then(result => {
+          if (result.body.status === 0) {
+            this.list = result.body.message
+          }
+        })
+      }
     }
   }
 </script>
@@ -91,6 +109,44 @@
 <style lang="scss" scoped>
     * {
         touch-action: pan-y;
+    }
+
+    .photo-list {
+        list-style: none;
+        margin: 0;
+        padding: 10px;
+        padding-bottom: 0;
+        li {
+            background-color: #ccc;
+            text-align: center;
+            margin-bottom: 10px;
+            box-shadow: 0 0 9px #999;
+            position: relative;
+            img {
+                width: 100%;
+                vertical-align: middle;
+            }
+            img[lazy="loading"] {
+                width: 40px;
+                height: 300px;
+                margin: auto;
+            }
+
+            .info {
+                color: white;
+                text-align: left;
+                position: absolute;
+                bottom: 0;
+                background-color: rgba(0, 0, 0, 0.4);
+                max-height: 84px;
+                .info-title {
+                    font-size: 14px;
+                }
+                .info-body {
+                    font-size: 13px;
+                }
+            }
+        }
     }
 </style>
 
