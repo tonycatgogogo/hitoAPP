950f802c86746894cb84f458ab7a0e45bf55ef96
tonycatgogogo <gonghaitao1@gmail.com>
2019-04-16 21:59:31 +0800

    2019.4.16提交by hito

--- src/components/Goods/GoodsDesc.vue
+++ src/components/Goods/GoodsDesc.vue
@@ -1,10 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-
-</body>
-</html>
+<template>
+    <div class="goodsdesc-container">
+        <h3>{{ info.title}}</h3>
+        <hr>
+        <div class="content" v-html="info.content"></div>
+    </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        info: {}
+      }
+    },
+    created() {
+      this.getGoodsDesc()
+    },
+    methods: {
+      getGoodsDesc() {
+        this.$http.get("api/goods/getdesc/" + this.$route.params.id).then(resule => {
+          if (result.body.status === 0) {
+            this.info = result.body.message[0]
+          }
+        })
+      }
+    }
+  }
+</script>
+<style lang="scss">
+    .goodsdesc-container {
+        padding: 4px;
+        h3 {
+            font-size: 16px;
+            color: #226aff;
+            text-align: center;
+            margin: 15px 0;
+        }
+        .content {
+            img {
+                width: 100%;
+            }
+        }
+    }
+</style>
